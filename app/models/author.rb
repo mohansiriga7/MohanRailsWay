4ce654a2eae1a693b97918c8bb60a800af22290d
mohansiriga7 <ms7.developer@gmail.com>
2017-05-02 12:55:42 +0530

    example for how to add custom errors to model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+  def validate!
+    errors.add(:name, :blank, message: "cannot be nil") if name.nil?
+  end
+
+  def show_errors
+    errors.each do |error|
+      puts error
+    end
+  end
 end
